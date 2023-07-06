53fc2c25b97d7a89cd7d7ba63a3386698dfe9089
Emmanuel Nsangalufu <ensangalufu@gmail.com>
2023-07-06 11:39:26 -0500

    Added new nodes

--- ep/src/ep/pipeline_registry.py
+++ ep/src/ep/pipeline_registry.py
@@ -1,7 +1,7 @@
 """Project pipelines."""
 from typing import Dict
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -10,4 +10,6 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": pipeline([])}
+    return {"__default__": Pipeline([
+            node(lambda x: x.describe(), inputs='pandas_iris_data', outputs=None),
+        ])}
