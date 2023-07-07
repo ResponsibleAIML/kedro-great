212f8a4dfe3fcb81a67515432d6466508bcbc9b4
Emmanuel Nsangalufu <ensangalufu@gmail.com>
2023-07-07 11:55:55 -0500

    Added sample pipeline

---
+++ example-pipeline/src/ep/pipelines/describe_pandas_iris/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'describe_pandas_iris'
+generated using Kedro 0.18.11
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([node(lambda x: x.describe(), inputs='pandas_iris_data', outputs=None),])
